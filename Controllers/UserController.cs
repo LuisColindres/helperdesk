1ae5589c4652ad0dcb00777fdcedc8b55d53b110
Luis Colindres <luis596@outlook.com>
2020-09-11 15:26:06 -0600

    Se agrego validacion por mensaje de texto

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HelperDesk.API.Controllers
 {
-    // [Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
