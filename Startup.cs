27118f9b4c944e1b954ebefac28cc419ccbc37d6
Luis Colindres <luiscolindres07@gmail.com>
2022-11-22 00:22:50 -0600

    Cambios de prueba

--- Startup.cs
+++ Startup.cs
@@ -133,7 +133,6 @@ namespace HelperDesk.API
                     };
                 });
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
